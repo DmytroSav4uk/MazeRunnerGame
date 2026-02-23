cac9fb06e28b8fce895c62e80a86c65d1e159fba
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2026-02-23 12:37:48 +0200

    +cookie popup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {NgcCookieConsentConfig, provideNgcCookieConsent} from 'ngx-cookieconsent';
+
+
+export const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'localhost:4200'
+  },
+  palette: {
+    popup: {
+      background: '#000'
+    },
+    button: {
+      background: '#f1d600'
+    }
+  },
+  theme: 'edgeless',
+  type: 'info'
+};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNgcCookieConsent(cookieConfig)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnDestroy, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgcCookieConsentService} from 'ngx-cookieconsent';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnDestroy{
   protected readonly title = signal('MazeRunnerGame');
+  private statusChangeSubscription!: Subscription;
+
+  constructor(private ccService: NgcCookieConsentService) {}
+
+  ngOnInit() {
+    this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
+      (event) => {
+
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.statusChangeSubscription.unsubscribe();
+  }
+
+
+
 }
